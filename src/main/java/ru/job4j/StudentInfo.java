0abe5af400b5d700783bed72fd2865dc72a81a37
Evgenij <kudrinevgenij@gmail.com>
2023-11-18 22:50:21 +0700

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Kudrin Evgenij");
+        System.out.println("19.06.1986");
+    }
+}
