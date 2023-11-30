bdf286fb30d531be7e3cef28273387cf654b0364
Evgenij <kudrinevgenij@gmail.com>
2023-11-30 22:02:05 +0700

    4.5. Отладка программы в IDEA [#159464]

--- src/main/java/ru/job4j/condition/SqArea.java
+++ src/main/java/ru/job4j/condition/SqArea.java
@@ -3,8 +3,9 @@ package ru.job4j.condition;
 public class SqArea {
     public static double square(double p, double k) {
         double h = p / (2 * (k + 1));
-        double rsl = h * h * k;
-        return rsl;
+        double w = h * k;
+        double s = w * h;
+        return s;
     }
 
     public static void main(String[] args) {
--- src/test/java/ru/job4j/condition/SqAreaTest.java
+++ src/test/java/ru/job4j/condition/SqAreaTest.java
@@ -6,6 +6,15 @@ import static org.assertj.core.api.Assertions.*;
 class SqAreaTest {
 
     @Test
+    void whenP4K1Square1() {
+        int expected = 1;
+        int p = 4;
+        int k = 1;
+        double out = SqArea.square(p, k);
+        assertThat(out).isCloseTo(expected, offset(0.01));
+    }
+
+    @Test
     void whenP6K2Square2() {
         int expected = 2;
         int p = 6;
