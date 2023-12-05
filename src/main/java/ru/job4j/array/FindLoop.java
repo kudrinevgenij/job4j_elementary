ce4cc9d1b29b796163460243ae8ce24208491357
Evgenij <kudrinevgenij@gmail.com>
2023-12-05 15:01:45 +0700

    6.3. Массив заполнен true или false [#53857 #464332]

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -11,4 +11,10 @@ public class FindLoop {
         }
         return rst;
     }
+
+    public static int indexInRange(int[] data, int el, int start, int finish) {
+        int rst = -1;
+
+        return rst;
+    }
 }
